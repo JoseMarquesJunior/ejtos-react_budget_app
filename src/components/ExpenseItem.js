b62bb1148d256b9f51435d45c51ec4c26430705c
JoseMarquesJunior <jmarques.oliveirajunior@gmail.com>
2024-01-04 15:20:48 -0500

    mensagem final

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -12,26 +12,31 @@ const ExpenseItem = (props) => {
         });
     };
 
-    const increaseAllocation = (name) => {
+    const adjustAllocation = (name, isIncrease) => {
         const expense = {
             name: name,
-            cost: 10,
+            cost: isIncrease ? 10 : -10, // Use a negative value for decrease
         };
 
         dispatch({
             type: 'ADD_EXPENSE',
             payload: expense
         });
-
-    }
+    };
 
     return (
         <tr>
-        <td>{props.name}</td>
-        <td>{currency}{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+            <td>{props.name}</td>
+            <td>{currency}{props.cost}</td>
+            <td>
+                <button className="btn btn-success" onClick={() => adjustAllocation(props.name, true)}>+</button>
+            </td>
+            <td>
+                <button className="btn btn-danger" onClick={() => adjustAllocation(props.name, false)}>-</button>
+            </td>
+            <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
+        
     );
 };
 
