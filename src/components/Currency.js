b5d0bcb34b5a27677ebcaeba5f3381946cccdce8
JoseMarquesJunior <jmarques.oliveirajunior@gmail.com>
2024-01-04 13:06:34 -0500

    alterando currency nos arquivos

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,15 +1,27 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
-const Currency = ({ currency, handleCurrencyChange }) => {
-    return (
-      <select value={currency} onChange={handleCurrencyChange}>
-        <option value="£">£ Pound</option>
-        <option value="$">$ Dollar</option>
-        <option value="€">€ Euro</option>
-        <option value="₹">₹ Ruppee</option>
-      </select>
-    );
-  };
+const Currency = () => {
+    const { currency, dispatch } = useContext(AppContext);
 
-export default Currency
+    const handleCurrencyChange = (event) => {
+        const newCurrency = event.target.value;
+        dispatch({
+            type: 'CHG_CURRENCY',
+            payload: newCurrency,
+        });
+    };
+
+    return (
+        <div className='btn btn-success dropdown-toggle'>
+            <select value={currency} onChange={handleCurrencyChange}>
+                <option value="£">£ Pound</option>
+                <option value="$">$ Dollar</option>
+                <option value="€">€ Euro</option>
+                <option value="₹">₹ Ruppee</option>
+            </select>
+        </div>
+    );
+};
+
+export default Currency;
