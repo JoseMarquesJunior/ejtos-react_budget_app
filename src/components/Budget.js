4eb2ab7fd4ee837ffa648bccc6046b558562ac44
JoseMarquesJunior <jmarques.oliveirajunior@gmail.com>
2024-01-04 12:33:30 -0500

    trabalho final

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -3,10 +3,22 @@ import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
     const { budget, dispatch } = useContext(AppContext);
+    const { expenses  } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
 
     const handleBudgetChange = (event) => {
         const value = event.target.value;
+        if (value > 20000) {
+            alert("The value cannot exceed 20,000 ");
+            return;
+        }
+        const totalExpenses = expenses.reduce((total, item) => {
+            return (total += item.cost);
+        }, 0);
+        if (value < totalExpenses){
+            alert("You cannot reduce the budget value lower than the spending. ");
+            return;
+        }
         setNewBudget(value);
         dispatch({
             type: 'SET_BUDGET',
